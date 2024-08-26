bd1c6d26d1974219e5e7de194679a0d3d1143299
Psychea13 <kea260209@gmail.com>
2024-08-26 14:55:54 +0300

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -3,8 +3,9 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
+        obj = object.__new__(cls)
         cls.houses_history.append(args[0])
-        return super().__new__(cls)
+        return obj
 
     def __init__(self, name, number_of_floors):
         self.name = name
